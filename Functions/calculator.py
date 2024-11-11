df1b69e62704faf331586c53b396def37ce243b6
JoeVA2020 <joevabraham2020@gmail.com>
2024-11-11 14:54:15 +0530

    Shortened calculator.py

--- Functions/calculator.py
+++ Functions/calculator.py
@@ -12,24 +12,18 @@ def div(num1,num2):
     print(sum)
 
 
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
 
 print(" 1.Addition\n2.Subtraction\n3.Multiplication\n4.Division")
 choice=int(input("Enter Choice : "))
 if choice==1:
-    num1 = int(input("Enter number 1: "))
-    num2 = int(input("Enter number 2: "))
     add(num1,num2)
 elif choice==2:
-    num1 = int(input("Enter number 1: "))
-    num2 = int(input("Enter number 2: "))
     sub(num1, num2)
 elif choice==3:
-    num1 = int(input("Enter number 1: "))
-    num2 = int(input("Enter number 2: "))
     mul(num1, num2)
 elif choice==4:
-    num1 = int(input("Enter number 1: "))
-    num2 = int(input("Enter number 2: "))
     div(num1, num2)
 else:
     print("Choose Valid option")
